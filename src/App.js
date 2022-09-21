a9210cab9939aa1f991b47f8a823ec09d53e2098
Adham-Niazy <adhamniazy@gmail.com>
2022-09-21 04:21:40 +0200

    Adjust Sky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
       </Canvas>
     </>
   );
